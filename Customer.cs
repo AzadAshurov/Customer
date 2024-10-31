0a3ade21044b6468ae4cfe95e9dd1ffe1b77424f
DevRufet <nesibovrufet47@gmail.com>
2024-10-31 19:12:26 +0400

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Customer
+{
+    internal class Customer
+    {
+        public static string jsonAddress = @"C:\Users\II novbe\Desktop\Customer\Data\customers.json";
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public static void Add(Customer customer)
+        {
+            using (StreamReader sr = new StreamReader(Program.jsonAddress)) ;
+
+            //string json = 
+            List<Customer> list = JsonConvert.DeserializeObject<List<Customer>>(jsonAddress);
+            list.Add(customer);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Customer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = @"C:\Users\II novbe\Desktop\Customer\Data";
+            string fileName = "Data";
+            Directory.CreateDirectory(address);
+            string filePath = Path.Combine(address, "customers.json");
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter sw = new StreamWriter(filePath)) ;
+            }
+            Customer.Add(new Customer() { FirstName = "Azad", Id = 5, LastName = "Bulba", PhoneNumber = "+666 66 666" });
+        }
+        public static string jsonAddress = @"C:\Users\II novbe\Desktop\Customer\Data\customers.json";
+        //File.WriteAllText(filePath, null);
+
+
+
+    }
+}
